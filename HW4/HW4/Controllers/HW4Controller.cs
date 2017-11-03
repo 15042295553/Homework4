e9399535449250c05ff167dcfabde6ca2f3325b2
Chunze-Liu <32441897+15042295553@users.noreply.github.com>
2017-11-03 11:45:33 +0800

    666

---
+++ HW4/HW4/Controllers/HW4Controller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW4.Controllers
+{
+    public class HW4Controller : Controller
+    {
+        // GET: HW4
+        public ActionResult Page1()
+        {
+            return View();
+        }
+        public ActionResult Page3()
+        {
+            return View();
+        }
+    }
+}
